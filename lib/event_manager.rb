abd7ed72b75140ae872fe1a28bc00bc5b7baac92
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 14:00:54 +0300

    enable csv library and display names of attendees without header row

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,10 @@
+require "csv"
 puts "Event Manager Initialized!"
 
 if File.exist? "event_attendees.csv"
-    lines = File.open("event_attendees.csv", "r").readlines
-    lines.each_with_index do |line, index|
-        next if index == 0
-        columns = line.split(",")
-        name = columns[2]
+    contents = CSV.open("event_attendees.csv", headers: true)
+    contents.each do |row|
+        name = row[2]
         puts name
     end
 else puts "File is not found"
