ec344bb5148800514d98f271b6b5fab4391de3bd
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 15:41:23 +0300

    make code cleaner in clean_zipcode function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,13 +2,7 @@ require "csv"
 puts "Event Manager Initialized!"
 
 def clean_zipcode(zipcode)
-    if zipcode.to_s.length < 5
-        zipcode.to_s.rjust(5, "0")
-    elsif
-        zipcode.to_s[0, 5]
-    else
-        zipcode
-    end
+        zipcode.to_s.rjust(5, "0")[0, 5]
 end
 
 if File.exist? "event_attendees.csv"
