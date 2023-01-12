93c28288357a524c9c6f5ec39c87a6234f046da6
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 15:31:16 +0300

    make our zip codes 5 digits in length: remove last symbols if zipcode length more than 5 digits, add zeros to the biginning of the word if zipcode length less than 5 digits

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,8 +9,16 @@ if File.exist? "event_attendees.csv"
     )
 
     contents.each do |row|
+        zip = row[:zipcode]
         name = row[:first_name]
-        puts name
+
+        if zip.to_s.length < 5
+            zip = zip.to_s.rjust(5, "0")
+        else
+            zip = zip.to_s[0, 5]
+        end
+
+        puts "#{name} - #{zip}"
     end
 else puts "File is not found"
 end
