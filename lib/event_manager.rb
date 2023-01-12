1ed3fdcf4c1fd9644d826d90d9f5b8926bd8ab6a
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 06:53:43 +0300

    display attendees' names skipping the head row

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,11 @@ puts "Event Manager Initialized!"
 
 if File.exist? "event_attendees.csv"
     lines = File.open("event_attendees.csv", "r").readlines
-    lines.each do |line|
-        puts line.split(",")[2]
+    lines.each_with_index do |line, index|
+        next if index == 0
+        columns = line.split(",")
+        name = columns[2]
+        puts name
     end
 else puts "File is not found"
 end
