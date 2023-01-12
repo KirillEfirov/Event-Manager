c23ca7cf5eabdd13d382062204efe7d5d4b338dc
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 06:45:26 +0300

    display names of attendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,8 @@ puts "Event Manager Initialized!"
 
 if File.exist? "event_attendees.csv"
     lines = File.open("event_attendees.csv", "r").readlines
-    puts lines
+    lines.each do |line|
+        puts line.split(",")[2]
+    end
 else puts "File is not found"
 end
