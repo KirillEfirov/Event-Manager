66d81b492785c10fcd88be38c4270e4d55010e8c
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-12 14:58:47 +0300

    convert headers to symbols

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,9 +2,14 @@ require "csv"
 puts "Event Manager Initialized!"
 
 if File.exist? "event_attendees.csv"
-    contents = CSV.open("event_attendees.csv", headers: true)
+    contents = CSV.open(
+        "event_attendees.csv", 
+        headers: true, 
+        header_converters: :symbol
+    )
+
     contents.each do |row|
-        name = row[2]
+        name = row[:first_name]
         puts name
     end
 else puts "File is not found"
