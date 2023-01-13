c545310f6224e737f91123afb70cf6f7c2dd7d0b
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-13 18:32:20 +0300

    display legislators by the name and zipcode using google-civic-api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,9 @@
 require "csv"
+require 'google/apis/civicinfo_v2'
 puts "Event Manager Initialized!"
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
         zipcode.to_s.rjust(5, "0")[0, 5]
@@ -16,7 +20,20 @@ if File.exist? "event_attendees.csv"
         zipcode = clean_zipcode(row[:zipcode])
         name = row[:first_name]
 
-        puts "#{name} - #{zipcode}"
+        begin
+            legislators = civic_info.representative_info_by_address(
+                address: zipcode,
+                levels: 'country',
+                roles: ['legislatorUpperBody', 'legislatorLowerBody']
+            )
+            legislators = legislators.officials
+        rescue
+            'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+        end
+        
+        puts "#{name} #{zipcode} #{legislators}"
     end
 else puts "File is not found"
 end
+
+
