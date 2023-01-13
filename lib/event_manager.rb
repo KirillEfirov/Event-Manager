abe14292efd4687c37d3c1a328d2deb2e659e650
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-13 19:13:55 +0300

    make function that displays legislator's names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,11 +2,30 @@ require "csv"
 require 'google/apis/civicinfo_v2'
 puts "Event Manager Initialized!"
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 def clean_zipcode(zipcode)
         zipcode.to_s.rjust(5, "0")[0, 5]
+end
+
+def legisplators_by_zipcode(zipcode)
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+    begin
+        legislators = civic_info.representative_info_by_address(
+            address: zipcode,
+            levels: 'country',
+            roles: ['legislatorUpperBody', 'legislatorLowerBody']
+        )
+        legislators = legislators.officials
+
+        legislators_names = legislators.map do |legislator|
+                legislator.name
+        end
+
+        legislators_names.join(", ")
+    rescue
+        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
 end
 
 if File.exist? "event_attendees.csv"
@@ -18,26 +37,7 @@ if File.exist? "event_attendees.csv"
 
     contents.each do |row|
         zipcode = clean_zipcode(row[:zipcode])
-        name = row[:first_name]
-
-        begin
-            legislators = civic_info.representative_info_by_address(
-                address: zipcode,
-                levels: 'country',
-                roles: ['legislatorUpperBody', 'legislatorLowerBody']
-            )
-            legislators = legislators.officials
-
-            legislators_names = legislators.map do |legislator|
-                    legislator.name
-            end
-        rescue
-            'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-        end
-
-        legislators_names = ["Not found"] if legislators_names.nil?
-        
-        puts "#{name} #{zipcode} #{legislators_names.join()}"
+        puts "#{row[:first_name]} - #{zipcode} - #{legisplators_by_zipcode(zipcode)}"
     end
 else puts "File is not found"
 end
